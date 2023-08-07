3048843fca1c5c02be8b467683cbba10c6815176
ammadeus <sumyk_exp@libero.it>
2023-08-07 20:58:55 +0200

    home work_11.1

--- homework1_10/homework_10_3_script.py
+++ homework1_10/homework_10_3_script.py
@@ -1,19 +1,18 @@
 import random
+import csv
 
 names_list = ["Anna", "Emma", "Mario", "Robert", "Giulia"]
 score_list = []
 
 for name in names_list:
-    count = 0
+    #count_ = 0
     for i in range(100):
         score = random.randint(0, 1000)
-        count += score  
-    score_list.append((name, count))  
+        #count_ += score  
+        score_list.append(score)  
 
 print(score_list)  
 
-
-import csv
 
 with open('homework_10_3_player_scores.csv', 'w', newline='') as csvfile:
     writer = csv.writer(csvfile)
