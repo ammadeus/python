238974aa86e949a42de855b8a09754ffa96b1897
ammadeus <sumyk_exp@libero.it>
2023-08-22 20:12:37 +0200

    keyboard questions about us dousn't work

--- teleg_bot/handlers/client.py
+++ teleg_bot/handlers/client.py
@@ -1,7 +1,10 @@
 from aiogram import types, Dispatcher
 from create_bot import dp, bot
-from keyboards import kb_client, tools_inline_keyboard, production_keyboard
+from keyboards import kb_client, tools_inline_keyboard, production_keyboard, learning_inline_keyboard, about_us_inline_keyboard,question_about_us_inline_keyboard
 from aiogram.types import ReplyKeyboardRemove
+from aiogram.dispatcher.filters import Text
+from aiogram.dispatcher import FSMContext
+
 
     
 
@@ -27,6 +30,23 @@ async def tools_command(message: types.Message):
 #@dp.message_handler(commands=["Production"])
 async def production_command(message: types.Message):
      await bot.send_message(message.from_user.id, "Please press one of the following button: ", reply_markup=production_keyboard)
+ # Production choice handler
+#async def handle_production_choice(message: types.Message):
+    #await message.answer("You selected: " + message.text)
+    #remove_markup = types.ReplyKeyboardRemove()
+    #await message.answer(reply_markup=remove_markup)
+    
+
+#@dp.message_handler(commands=["Learning"])
+async def learning_command(message: types.Message):
+     await bot.send_message(message.from_user.id, "Please press one of the following button: ", reply_markup=learning_inline_keyboard)
+     
+     
+#@dp.message_handler(commands=["About Us"])
+async def about_us_command(message: types.Message):
+     await bot.send_message(message.from_user.id, "Please press one of the following button: ", reply_markup=about_us_inline_keyboard)
+
+
 
         
 # decor. con gg di lavoro
@@ -39,7 +59,24 @@ async def open_command(message : types.Message):
 async def adress_command(message : types.Message):
     await bot.send_message(message.from_id, 'Kharkiv...')
     
+# keyboard question about us(About Us)
+#@dp.message_handler(Text(equal='questions about us'))
+#async def question_about_us(message : types.Message):
+    #await bot.send_message(message.from_id, "Answer :  ")
+#@dp.callback_query_handler(lambda query: query.data == 'questions about us')
+#async def question_about_us(callback_query: types.CallbackQuery):
+    #await bot.send_message(callback_query.from_user.id, "Here's a new inline keyboard:", reply_markup=question_about_us_inline_keyboard)
+async def show_question_about_us_inline_keyboard(callback_query: types.CallbackQuery):
+    if callback_query.data == "questions about us":
+        await bot.send_message(
+            callback_query.from_user.id,
+            "Here's a new inline keyboard for questions about us:",
+            reply_markup=question_about_us_inline_keyboard,
+        )
+
+
     
+     
 
     
 
@@ -49,5 +86,11 @@ def register_handers_client(dp : Dispatcher):
     dp.register_message_handler(command_start, commands=['start', 'help'])
     dp.register_message_handler(tools_command, commands=['Tools'])
     dp.register_message_handler(production_command, commands=["Production"])
+    #dp.register_message_handler(handle_production_choice, lambda message: message.text in ["/express виготовлення килимів", "/стандартне виготовлення килимів"])
+    dp.register_message_handler(learning_command, commands=["Learning"])
+    dp.register_message_handler(about_us_command, commands=["About"])
     dp.register_message_handler(open_command, commands=["Open"]) 
-    dp.register_message_handler(adress_command, commands=["Address"]) 
+    dp.register_message_handler(adress_command, commands=["Address"])
+    dp.register_callback_query_handler(show_question_about_us_inline_keyboard, lambda query: query.data == 'questions about us')
+    #dp.register_message_handler(handle_production_choice)
+    
--- teleg_bot/keyboards/client_kb.py
+++ teleg_bot/keyboards/client_kb.py
@@ -10,7 +10,6 @@ b5 = KeyboardButton('/Address')
 b6 = KeyboardButton('/About Us')
 
 kb_client  = ReplyKeyboardMarkup(resize_keyboard=True,one_time_keyboard=True)# scompare dopo che premi su
-
 kb_client.row(b1, b2, b3).row(b4, b5, b6)
 
 # keyboard inline del tasto tools
@@ -19,7 +18,7 @@ button2 = InlineKeyboardButton(text="знижки на інструменти", 
 button3 = InlineKeyboardButton(text="корисні поради(в подарунок налаштування) та гарантії", callback_data="button3")
 button4 = InlineKeyboardButton(text="доставка", callback_data="button4")
 
-tools_inline_keyboard = InlineKeyboardMarkup()
+tools_inline_keyboard = InlineKeyboardMarkup(resize_keyboard=True)
 tools_inline_keyboard.row(button1, button2).row(button3, button4) 
 
 #def tools_inline_keyboard():# SECONDA VARIAZIONE DEL keyboard inline del tasto tools
@@ -31,9 +30,41 @@ tools_inline_keyboard.row(button1, button2).row(button3, button4)
     #inline_keyboard.add(button1, button2)
 
     #return inline_keyboard
- # keybord del tasto Production 
-but1 = KeyboardButton('/express виготовлення килимів')
-but2 = KeyboardButton('/стандартне виготовлення килимів')
+ # keybord del tasto Production diventa inline
+#but1 = KeyboardButton('/express виготовлення килимів')
+#but2 = KeyboardButton('/стандартне виготовлення килимів')
+but1 = InlineKeyboardButton(text="express виготовлення килимів", callback_data="but1")
+but2 = InlineKeyboardButton(text="стандартне виготовлення килимів", callback_data="but2")
 
-production_keyboard = ReplyKeyboardMarkup(resize_keyboard=True,one_time_keyboard=True)
+production_keyboard = InlineKeyboardMarkup(resize_keyboard=True)
 production_keyboard.row(but1).row(but2)
+
+# inline keyboard del tasto Learning
+butt1 = InlineKeyboardButton(text="онлайн курс", callback_data="butt1")
+butt2 = InlineKeyboardButton(text="майстер клас", callback_data="butt2")
+
+learning_inline_keyboard = InlineKeyboardMarkup(resize_keyboard=True) 
+learning_inline_keyboard.row(butt1).row(butt2)
+
+# inline keyboard del tasto About Us
+bt1 = InlineKeyboardButton(text="посилання на соц мереж", callback_data="bt1")
+bt2 = InlineKeyboardButton(text="questions about us", callback_data="bt2")
+bt3 = InlineKeyboardButton(text="відгуки про нас", callback_data="bt3")
+
+about_us_inline_keyboard = InlineKeyboardMarkup(resize_keyboard=True)
+about_us_inline_keyboard.row(bt1).row(bt2).row(bt3)
+
+# inline keyboard question about us(About Us) 
+#abt1 = InlineKeyboardButton(text="question N.1", callback_data="abt1")
+#abt2 = InlineKeyboardButton(text="question N.2", callback_data="abt2")
+#abt3 = InlineKeyboardButton(text="question N.3", callback_data="abt3")
+
+#question_about_us_inline_keyboard = InlineKeyboardMarkup(resize_keyboard=True)
+#question_about_us_inline_keyboard = types.InlineKeyboardButton("Show New Inline Keyboard", callback_data="question_about_us_inline_keyboard")
+# Inline keyboard per la nuova inline keyboard
+#question_about_us_inline_keyboard.add(types.InlineKeyboardButton("New Button 1", callback_data="new_button_1"))
+#question_about_us_inline_keyboard.add(types.InlineKeyboardButton("New Button 2", callback_data="new_button_2"))
+
+question_about_us_inline_keyboard = InlineKeyboardMarkup()
+question_about_us_inline_keyboard.add(InlineKeyboardButton("New Button 1", callback_data="new_button_1"))
+question_about_us_inline_keyboard.add(InlineKeyboardButton("New Button 2", callback_data="new_button_2"))
